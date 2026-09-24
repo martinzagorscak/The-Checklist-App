a2da6218d55735fe4a1a687a20a4504ea52f2334
martinzagorscak <martin.zagorscak25@gmail.com>
2026-09-24 16:46:12 +0200

    removed unused code

--- app/src/main/java/com/example/thechecklistapp/ui/screens/MainChecklistScreen.kt
+++ app/src/main/java/com/example/thechecklistapp/ui/screens/MainChecklistScreen.kt
@@ -4,11 +4,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -28,22 +24,9 @@ fun MainChecklistScreen(
     callbacks: MainChecklistScreenCallbacks,
     modifier: Modifier = Modifier,
 ) {
-    val snackbarHostState = remember { SnackbarHostState() }
-
-    LaunchedEffect(Unit) {
-        snackbarHostState.showSnackbar(
-            message = "Snackbar text",
-            duration = SnackbarDuration.Short,
-            withDismissAction = true,
-        )
-    }
-
     Scaffold(
         topBar = {
             TopBar(title = stringResource(R.string.checklist_screen_title))
-        },
-        snackbarHost = {
-            SnackbarHost(hostState = snackbarHostState)
         },
         modifier = modifier.fillMaxSize(),
     ) { paddingValues ->
